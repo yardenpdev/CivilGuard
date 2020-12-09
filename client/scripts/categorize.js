ec5c3ba54fb278d3bb7144024154c16ba266ff63
Noam Rosenthal <noam@webkit.org>
2020-12-09 11:40:27 +0200

    First take at categorize

--- client/scripts/categorize.js
+++ client/scripts/categorize.js
@@ -1,33 +1,50 @@
 import * as api from './api.js'
 
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener('DOMContentLoaded', () => {
     const categorizeForm = document.querySelector('#categorize')
     const table = categorizeForm.querySelector('table > tbody')
     const issueTemplate = categorizeForm.querySelector('tbody tr')
+    const fromDate = document.querySelector('#fromDate')
+    const toDate = document.querySelector('#toDate')
+    fromDate.value = new Date().toISOString().substr(0, 10)
+    toDate.value = new Date(new Date().valueOf() + 1000 * 60 * 60 * 24 * 7).toISOString().substr(0, 10)
     issueTemplate.remove()
-    const startDate = new Date()
-    const endDate = new Date(new Date().valueOf() + 1000 * 60 * 60 * 24 * 7)
-    const sessions = await api.getKnessetData({startDate, endDate})
-    const subjects = await api.getSubjects()
-    for (const session of sessions) {
-        for (const item of session.items) {
-            const element = issueTemplate.cloneNode(true)
-            element.querySelector('.issue').innerText = item.title
-            const checkboxTemplate = element.querySelector('span.subject')
-            const subjectArea = element.querySelector('.subjects') 
-            checkboxTemplate.remove()
-            for (const subject of subjects) {
-                const span = checkboxTemplate.cloneNode(true)
-                const checkbox = span.querySelector('input')
-                const label = span.querySelector('label span')            
-                label.innerText = subject
-                checkbox.checked = issue.subjects.includes(subject)
-                checkbox.onchange = () => api.toggleSubject(issue, subject, checkbox.checked)
-                subjectArea.appendChild(span)
+    const render = async() => {
+        table.innerHTML = ''
+        const startDate = new Date(fromDate.value)
+        const endDate = new Date(toDate.value)
+        const sessions = await api.getKnessetData({startDate, endDate})
+        const subjects = await api.getSubjects()
+        console.log(sessions)
+        for (const {session, items, committee} of sessions) {
+            for (const item of items) {
+                const element = issueTemplate.cloneNode(true)
+                element.querySelector('.committee').innerText = committee.Name
+                element.querySelector('.date').innerText = new Date(session.StartDate).toLocaleString('he-IL')
+                element.querySelector('.issue').innerText = item.Name
+                if (session.SessionUrl)
+                    element.querySelector('.issue').href = session.SessionUrl
+                const checkboxTemplate = element.querySelector('span.subject')
+                const subjectArea = element.querySelector('.subjects') 
+                checkboxTemplate.remove()
+                for (const subject of subjects) {
+                    const span = checkboxTemplate.cloneNode(true)
+                    const checkbox = span.querySelector('input')
+                    const label = span.querySelector('label span')            
+                    label.innerText = subject
+    //                checkbox.checked = issue.subjects.includes(subject)
+                    checkbox.onchange = () => api.toggleSubject(issue, subject, checkbox.checked)
+                    subjectArea.appendChild(span)
+                }
+
+                table.appendChild(element)
             }
 
-            table.appendChild(element)
         }
-
     }
+
+    fromDate.onchange = render
+    toDate.onchange = render
+    render()
+
 })
