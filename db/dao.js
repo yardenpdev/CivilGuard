a8a2775f776e2c54a3bf15ce4ceec9e54f3e7236
Noam Rosenthal <noam@webkit.org>
2020-12-13 09:22:48 +0200

    Add table creation to client

--- db/dao.js
+++ db/dao.js
@@ -1,25 +1,44 @@
 const { Client } = require('pg')
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-})
+async function createDB() {
+    const pgClient = new Client({
+        connectionString: process.env.DATABASE_URL,
+    })
 
-client.connect()
+    await pgClient.connect()
+    await pgClient.query(
+        "CREATE TABLE IF NOT EXISTS remarks (user_id  varchar(80), text text, time_inserted date, session_id varchar(8));"
+    )
 
+    await pgClient.query(
+        "CREATE TABLE IF NOT EXISTS users (user_id  varchar(80) PRIMARY KEY, name varchar(80), photo varchar(256), email varchar(80), date_created date, subjects varchar(32)[], last_updated date);"
+    )
 
+    await pgClient.query(
+        "CREATE TABLE IF NOT EXISTS session_subject (session  varchar(80), subject varchar(80));"
+    )
+
+    await pgClient.query(
+        "CREATE TABLE IF NOT EXISTS subject (title VARCHAR(80) PRIMARY KEY);"
+    )
+
+    return pgClient
+}
+
+const db = createDB()
+
+const query = async (sql, args) => (await db).query(sql, args)
 class DAO {
 
     async getRemarksOfSession(session_id) {
         console.log(`DAO - getRemarksOfSession ${session_id}`);
-        const res = await client
-            .query('SELECT remarks.user_id as user_id, name, photo, text, time_inserted from remarks INNER JOIN users ON remarks.user_id=users.user_id where remarks.session_id = $1', [session_id])
+        const res = await query('SELECT remarks.user_id as user_id, name, photo, text, time_inserted from remarks INNER JOIN users ON remarks.user_id=users.user_id where remarks.session_id = $1', [session_id])
         return res.rows
     }
 
     async addRemarkToSession(user_id, session_id, remark) {
         console.log('DAO - addRemarkToSession');
         var date_added = new Date();
-        return client
-        .query('INSERT INTO remarks(user_id, text, time_inserted, session_id) VALUES($1, $2, $3, $4)',
+        return query('INSERT INTO remarks(user_id, text, time_inserted, session_id) VALUES($1, $2, $3, $4)',
                  [user_id, remark, date_added, session_id])
         .then(res => res.rowCount == 1)
         .catch(e => { console.error(e.stack); return false;});
@@ -27,8 +46,7 @@ class DAO {
 
     async deleteRemark(user_id, session_id, remark) {
         console.log(`DAO - deleteRemark ${user_id} ${session_id} ${remark}`);
-        return client
-        .query('DELETE FROM remarks WHERE user_id=$1 AND session_id=$2 AND text=$3',
+        return query('DELETE FROM remarks WHERE user_id=$1 AND session_id=$2 AND text=$3',
                  [user_id, session_id, remark])
         .then(res => res.rowCount == 1)
         .catch(e => { console.error(e.stack); return false;});
@@ -36,14 +54,14 @@ class DAO {
 
     async setSubjectsToUser(user_id, subjects) {
         console.log('DAO - addSubjectsToUser');
-        const res = await client.query("UPDATE users SET subjects = $1 WHERE user_id = $2",
+        const res = await query("UPDATE users SET subjects = $1 WHERE user_id = $2",
                  [subjects, user_id])
         return res.rowCount === 1
     }
 
     async getUserSubjects(user_id) {
         console.log('DAO - getUserSubjects')
-        const response = await client.query('SELECT * from users where user_id = $1', [user_id])
+        const response = await query('SELECT * from users where user_id = $1', [user_id])
         return response.rows.length > 0 ? response.rows[0].subjects : []
     }
     
@@ -51,11 +69,11 @@ class DAO {
         console.log('DAO - insertUser');
         var date_added = new Date();
 
-        const exists = await client.query('SELECT user_id FROM users WHERE user_id=$1', [id])
+        const exists = await query('SELECT user_id FROM users WHERE user_id=$1', [id])
         if (exists.rowCount === 1)
             return
 
-        const res = await client.query('INSERT INTO users(user_id, name, email, photo, date_created, subjects, last_updated) VALUES($1, $2, $3, $4, $5, $6, $7)',
+        const res = await query('INSERT INTO users(user_id, name, email, photo, date_created, subjects, last_updated) VALUES($1, $2, $3, $4, $5, $6, $7)',
                     [id, name, email, photo, date_added, [], date_added])
         return res.rowCount == 1
     }
@@ -78,26 +96,26 @@ class DAO {
 
     async getSubjectsBySession(session_id) {
         console.log('DAO - getSessionSubjects')
-        const response = await client.query('SELECT subject from session_subject where session = $1', [session_id])
+        const response = await query('SELECT subject from session_subject where session = $1', [session_id])
         return response.rows.map(row => row.subject)
     }
 
     async getAllSubjects() {
         console.log('DAO - getAllSubjects')
-        const response = await client.query('SELECT * from subject')
+        const response = await query('SELECT * from subject')
         console.log(response)
         return response.rows.map(s => s.title)
     }
 
     async addSubject(subject) {
         console.log('DAO - addSubject')
-        const response = await client.query('INSERT INTO subject (title) VALUES($1)', [subject])
+        const response = await query('INSERT INTO subject (title) VALUES($1)', [subject])
         return response.rows
     }
 
     async deleteSubject(subject) {
         console.log('DAO - addSubject')
-        const response = await client.query('DELETE FROM subject WHERE title=$1', [subject])
+        const response = await query('DELETE FROM subject WHERE title=$1', [subject])
         if (response.rowCount !== 1)
             return false
 
@@ -110,7 +128,7 @@ class DAO {
 
     async getSessionsBySubject(subjects) {
         console.log('DAO - getSessionsOfSubjects')
-        const response = await client.query('SELECT session from session_subject where subject = ANY($1)', [subjects])
+        const response = await query('SELECT session from session_subject where subject = ANY($1)', [subjects])
         return response.rows
     }
 
